c8619862e5778bae7dd3a592e6f4ce91b84345ce
Jose Antonio Palacios Ramirez <joseantpalaciosramirez@gmail.com>
2018-12-28 16:08:36 +0100

    ADD: Connect and check if socket server is connected

--- basic-fe/src/app/app.component.ts
+++ basic-fe/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from './services/websocket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'basic';
+export class AppComponent implements OnInit {
+  constructor(
+    public wsService: WebsocketService
+  ) {}
+
+  ngOnInit() {
+  }
+
 }
